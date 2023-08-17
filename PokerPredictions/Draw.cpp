5d44e7e4f28ae3473e28ebf6f5c78ac1acf2bb47
Odinn <kdcelli420@gmail.com>
2023-08-16 19:39:33 -0600

    Fixed Pair Prediction. Added more types of predictions. will need to start figuring out how to store output and where.

--- PokerPredictions/Draw.cpp
+++ PokerPredictions/Draw.cpp
@@ -2,11 +2,11 @@
 
 #include "PairPrediction.h"
 
-bool isPair(const Card& card1, const Card& card2) {
+bool PairPredictions::isPair(const Card& card1, const Card& card2) {
     return card1.getRank() == card2.getRank();
 }
 
-double estimatePairProbability(int simulations) {
+double PairPredictions::estimatePairProbability(int simulations) {
     int pairCount = 0;
 
     for (int i = 0; i < simulations; i++) {
@@ -16,18 +16,121 @@ double estimatePairProbability(int simulations) {
         Card card1 = deck.deal();
         Card card2 = deck.deal();
 
-        if (isPair(card1, card2)) {
+        if (PairPredictions::isPair(card1, card2)) {
             pairCount++;
         }
     }
 
     return static_cast<double>(pairCount) / simulations;
 }
-
+double PairPredictions::estimateFlushProbability(int simulations) {
+	int flushCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			flushCount++;
+		}
+	}
+	return static_cast<double>(flushCount) / simulations;
+}
+double PairPredictions::estimateStraightProbability(int simulations) {
+	int straightCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			straightCount++;
+		}
+	}
+	return static_cast<double>(straightCount) / simulations;
+}
+double PairPredictions::estimateThreeOfAKindProbability(int simulations) {
+	int threeOfAKindCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			threeOfAKindCount++;
+		}
+	}
+	return static_cast<double>(threeOfAKindCount) / simulations;
+}
+double PairPredictions::estimateTwoPairProbability(int simulations) {
+	int twoPairCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			twoPairCount++;
+		}
+	}
+	return static_cast<double>(twoPairCount) / simulations;
+}
+double PairPredictions::estimateFullHouseProbability(int simulations) {
+	int fullHouseCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			fullHouseCount++;
+		}
+	}
+	return static_cast<double>(fullHouseCount) / simulations;
+}
+double PairPredictions::estimateFourOfAKindProbability(int simulations) {
+	int fourOfAKindCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			fourOfAKindCount++;
+		}
+	}
+	return static_cast<double>(fourOfAKindCount) / simulations;
+}
+double PairPredictions::estimateStraightFlushProbability(int simulations) {
+	int straightFlushCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			straightFlushCount++;
+		}
+	}
+	return static_cast<double>(straightFlushCount) / simulations;
+}
+double PairPredictions::estimateRoyalFlushProbability(int simulations) {
+	int royalFlushCount = 0;
+	for (int i = 0; i < simulations; i++) {
+		Deck deck;
+		deck.shuffle();
+		Card card1 = deck.deal();
+		Card card2 = deck.deal();
+		if (PairPredictions::isPair(card1, card2)) {
+			royalFlushCount++;
+		}
+	}
+	return static_cast<double>(royalFlushCount) / simulations;
+}
 //Example of how to use the pair prediction
-void runSPairimluations() {
+void PairPredictions::runSPairimluations() {
     const int SIMULATIONS = 25000000;  // Adjust as needed
-    double probability = estimatePairProbability(SIMULATIONS);
+    double probability = PairPredictions::estimatePairProbability(SIMULATIONS);
     std::cout << "Estimated probability of getting a pair in hole cards (after " << SIMULATIONS << " simulations): " << probability << std::endl;
 }
 
@@ -58,10 +161,11 @@ void Drawing::Draw()
 			ImGui::Separator();
 			ImGui::Text("Pair Prediction");
 			ImGui::Separator();
-			ImGui::Text("Estimated probability of getting a pair in hole cards (after 25,000 simulations): %f", estimatePairProbability(25000));
-
+			ImGui::Text("Estimated probability of getting a pair in hole cards (after 25,000 simulations): %f", PairPredictions::estimatePairProbability(25000));
 			ImGui::Separator();
-
+			ImGui::Text("Flush Prediction");
+			ImGui::Separator();
+			ImGui::Text("Estimated probability of getting a flush in hole cards (after 25,000 simulations): %f", PairPredictions::estimateFlushProbability(25000));
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 		}
 		ImGui::End();
--- PokerPredictions/PairPrediction.h
+++ PokerPredictions/PairPrediction.h
@@ -4,5 +4,14 @@ namespace PairPredictions {
 
     bool isPair(const Card& card1, const Card& card2);
     double estimatePairProbability(int simulations);
+    double estimateFlushProbability(int simulations);
+    double estimateStraightProbability(int simulations);
+    double estimateThreeOfAKindProbability(int simulations);
+    double estimateTwoPairProbability(int simulations);
+    double estimateFullHouseProbability(int simulations);
+    double estimateFourOfAKindProbability(int simulations);
+    double estimateStraightFlushProbability(int simulations);
+    double estimateRoyalFlushProbability(int simulations);
+
     void runSPairimluations();
 }
